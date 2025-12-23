c60678456ebdd7b46009833c39062eecd1c26d08
AM TEST <amani.bousselidj@univ-constantine2.dz>
2025-12-23 06:38:21 +0100

    Add home page to backend and disable DEBUG logging

--- backend/future_smile/urls.py
+++ backend/future_smile/urls.py
@@ -9,8 +9,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('clinic.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
