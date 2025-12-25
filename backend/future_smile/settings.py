37e9bc00278dd4695378dd0bb83c0ac259966a16
AM TEST <amani.bousselidj@univ-constantine2.dz>
2025-12-25 11:58:18 +0100

    Change DEFAULT_PERMISSION_CLASSES to AllowAny for public API access

--- backend/future_smile/settings.py
+++ backend/future_smile/settings.py
@@ -196,7 +196,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',  # Allow unauthenticated access for GET
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
