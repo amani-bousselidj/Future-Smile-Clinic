4eb736356622aa64720147efac83d97762757733
AM TEST <amani.bousselidj@univ-constantine2.dz>
2026-01-07 08:55:28 +0100

    feat(popular): mobil3e horizontalff swipe cards14#

--- src/components/HeroTooth.tsx
+++ src/components/HeroTooth.tsx
@@ -1,7 +1,6 @@
 ﻿"use client";
 
 import React, { useEffect, useRef } from "react";
-import Link from "next/link";
 import { Header } from "./Header";
 import styles from "./HeroTooth.module.css";
 
